b3b9b496b7a446da7b6b0d789fa35bcd7a9128de
NoelMirandaDev <nymiranda1999@gmail.com>
2024-11-26 13:03:03 -0600

    Pushed on 11/26/2024

---
+++ module-9/Miranda_Mod9_3.js
@@ -0,0 +1,15 @@
+/*JavaScript for Miranda_Mod9_3.html*/
+
+/* Created function for easy calling and readability on html document */
+function changeSpanPlaceholder(id, variable) {
+    document.getElementById(id).innerHTML = variable;
+}
+
+/* Variables with values to replace span element placeholders */
+var firstVariableValue = "Chicago,";
+
+var secondVariableValue = "extraordinary";
+
+var thirdVariableValue = "genuinely";
+
+var fourthVariableValue = "Jennifer,";
